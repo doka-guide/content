aecf571ca05222ff021fef26b86f93d16ccebad2
Nikolai Lopin <code@lopin.me>
2021-03-21 23:27:56 +0100

    исправить отступы

--- .github/scripts/sort.js
+++ .github/scripts/sort.js
@@ -5,4 +5,4 @@ const SPELLER_PATH = path.resolve(__dirname, '../../.yaspeller.json')
 const json = require(SPELLER_PATH)
 json.dictionary.sort((a, b) => a.localeCompare(b))
 
-fs.writeFileSync(SPELLER_PATH, JSON.stringify(json, null, 2))
+fs.writeFileSync(SPELLER_PATH, JSON.stringify(json, null, 2) + '\n')
