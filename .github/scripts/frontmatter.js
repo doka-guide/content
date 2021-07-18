95be8381e74d1b1db9a779f77392278ed37cbdbf
Igor Korovchenko <igsekor@gmail.com>
2021-07-18 21:49:39 +0300

    Пробует черновик теста

--- .github/scripts/frontmatter.js
+++ .github/scripts/frontmatter.js
@@ -4,6 +4,41 @@ const rawMeta = fs.readFileSync('result.json');
 
 const commonMeta = JSON.parse(rawMeta);
 
-for (const fileMeta in commonMeta) {
-  console.log(`${commonMeta[fileMeta].title}`);
+for (const fileName in commonMeta) {
+  const fileMeta = commonMeta[fileName]
+  if (fileMeta.hasOwnProperty('title')
+    && fileMeta.hasOwnProperty('tags')
+    && fileMeta.hasOwnProperty('authors')
+    && fileMeta.hasOwnProperty('contributors')
+    && fileMeta.hasOwnProperty('summary')
+  ) {
+    console.log(`В файле '${fileName}' есть все необходимые поля`)
+  }
+
+  const metaKeys = Object.keys(fileMeta)
+  for (let i = 0; i < metaKeys.length; i++) {
+    switch (metaKeys[i]) {
+      case 'title':
+        if (i !== 0) {
+          console.error(`Field 'title' is not in right place!`);
+        }
+      case 'tags':
+        if (i !== 1) {
+          console.error(`Field 'tags' is not in right place!`);
+        }
+      case 'authors':
+        if (i !== 2) {
+          console.error(`Field 'authors' is not in right place!`);
+        }
+      case 'contributors':
+        if (i !== 3) {
+          console.error(`Field 'contributors' is not in right place!`);
+        }
+      case 'summary':
+        if (i !== 4) {
+          console.error(`Field 'summary' is not in right place!`);
+        }
+    }
+  }
+  console.log(`В файле '${fileName}' в правильном порядке`)
 }
