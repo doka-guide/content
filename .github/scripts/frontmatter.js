5eaadaf61005c86bcd6f66cb4e945909b6a56bbb
Igor Korovchenko <igsekor@gmail.com>
2021-07-18 20:40:14 +0300

    Меняет выражение для руглярки

--- .github/scripts/frontmatter.js
+++ .github/scripts/frontmatter.js
@@ -1,16 +1,7 @@
 const fs = require('fs');
 
-const rawHtml = fs.readFileSync('html.json');
-const rawCss = fs.readFileSync('css.json');
-const rawJs = fs.readFileSync('js.json');
-const rawTools = fs.readFileSync('tools.json');
+const rawMeta = fs.readFileSync('meta.json');
 
-const metaFromHtml = JSON.parse(rawHtml);
-const metaFromCss = JSON.parse(rawCss);
-const metaFromJs = JSON.parse(rawJs);
-const metaFromTools = JSON.parse(rawTools);
+const metaFromMeta = JSON.parse(rawMeta);
 
-console.log(metaFromHtml);
-console.log(metaFromCss);
-console.log(metaFromJs);
-console.log(metaFromTools);
+console.log(metaFromMeta);
