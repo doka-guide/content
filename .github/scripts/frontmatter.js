f960ea906adf0f1b55406da0b1614994d1f9e48f
Igor Korovchenko <igsekor@gmail.com>
2021-07-18 20:34:04 +0300

    Пробует разделение на категории

--- .github/scripts/frontmatter.js
+++ .github/scripts/frontmatter.js
@@ -1,5 +1,16 @@
 const fs = require('fs');
 
-const rawdata = fs.readFileSync('../../result.json');
-const metaFromFiles = JSON.parse(rawdata);
-console.log(metaFromFiles);
+const rawHtml = fs.readFileSync('../../html.json');
+const rawCss = fs.readFileSync('../../css.json');
+const rawJs = fs.readFileSync('../../js.json');
+const rawTools = fs.readFileSync('../../tools.json');
+
+const metaFromHtml = JSON.parse(rawHtml);
+const metaFromCss = JSON.parse(rawCss);
+const metaFromJs = JSON.parse(rawJs);
+const metaFromTools = JSON.parse(rawTools);
+
+console.log(metaFromHtml);
+console.log(metaFromCss);
+console.log(metaFromJs);
+console.log(metaFromTools);
