a0a9be32f9fb16b21ea12077139a087f46d005bc
Igor Korovchenko <igsekor@gmail.com>
2021-07-18 20:37:40 +0300

    Меняет пути

--- .github/scripts/frontmatter.js
+++ .github/scripts/frontmatter.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-const rawHtml = fs.readFileSync('../../html.json');
-const rawCss = fs.readFileSync('../../css.json');
-const rawJs = fs.readFileSync('../../js.json');
-const rawTools = fs.readFileSync('../../tools.json');
+const rawHtml = fs.readFileSync('html.json');
+const rawCss = fs.readFileSync('css.json');
+const rawJs = fs.readFileSync('js.json');
+const rawTools = fs.readFileSync('tools.json');
 
 const metaFromHtml = JSON.parse(rawHtml);
 const metaFromCss = JSON.parse(rawCss);
