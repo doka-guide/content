d079e169f3edd2254367e0c24300937df4577e81
Igor Korovchenko <igsekor@gmail.com>
2021-07-18 21:11:00 +0300

    Пробует экранирование

--- .github/scripts/frontmatter.js
+++ .github/scripts/frontmatter.js
@@ -1,16 +1,8 @@
 const fs = require('fs');
 
-const rawHtml = fs.readFileSync('html.json');
-const rawCss = fs.readFileSync('css.json');
-const rawJs = fs.readFileSync('js.json');
-const rawTools = fs.readFileSync('tools.json');
+const rawMeta = fs.readFileSync('result.json');
 
-const metaFromHtml = JSON.parse(rawHtml);
-const metaFromCss = JSON.parse(rawCss);
-const metaFromJs = JSON.parse(rawJs);
-const metaFromTools = JSON.parse(rawTools);
-
-const commonMeta = Object.assign(metaFromCss, metaFromHtml, metaFromJs, metaFromTools);
+const commonMeta = JSON.parse(rawMeta);
 
 for (const fileMeta in commonMeta) {
   console.log(`${commonMeta[fileMeta].title}`);
