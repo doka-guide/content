ca6a4f615a538c2e3954a0925016c0481a76f1d9
Igor Korovchenko <igsekor@gmail.com>
2021-07-25 15:03:04 +0300

    Добавляет скрипт для сборки индекса

---
+++ .github/scripts/search-index.js
@@ -0,0 +1,72 @@
+const fs = require('fs')
+const algoliaClient = require('algoliasearch')
+
+const rawSearch = fs.readFileSync('search.json')
+const commonSearch = JSON.parse(rawSearch)
+const algoliaIndex = []
+
+const patternsForEntities = {
+  header2:    /## .+\n/g,                                 // Заголовки второго уровня
+  header3:    /### .+\n/g,                                // Заголовки третьего уровня
+  header4:    /#### .+\n/g,                               // Заголовки четвёртого уровня
+  links:      /[ ]\[.+\]\(.\)/g,                          // Ссылки
+  images:     /[!]\[.+\]\(.\)/g,                          // Подписи к картинкам
+  lists:      /[1-9-] .+/g,                               // Элементы списка
+  bold:       /\*\*(.|\n)*\*\*/g,                         // Выделение полужирным шрифтом
+  italic:     /_((?![<>{}]).|\n)*_/g,                     // Выделение наклонным шрифтом
+  callouts:   /:::(.|\n)+:::/g,                           // Блоки callout
+  paragraphs: /^(?![#>\-*\d ])((?![#>\-*\d ]).+\n?)+/gm,  // Параграфы
+}
+
+const
+
+const commonReplacement = /([#]+ |:::|callout |```|\*{2,2}|\n|_|\!\[|\[|\]|\([.:/a-z]+\))/g
+
+const getEntitiesFromContent = (text, patterns) => {
+  text = text.replace(/---(.|\n)*---\n/g, '') // Вырезаем мету
+  text = text.replace(/```(.|\n)*```\n/g, '') // Вырезаем код
+  const output = {}
+  for (const field in patterns) {
+    let array = text.match(patterns[field])
+    if (array !== null && typeof array === 'object' && array.length > 0) {
+      array = array.map(element => {
+        return element.replace(commonReplacement, '')
+      })
+      output[field] = array
+    } else {
+      output[field] = []
+    }
+  }
+  return output
+}
+
+const getPractice = (path) => {
+  const output = []
+  if (fs.existsSync(path)) {
+    const files = fs.readdirSync(path, { encoding: 'utf8' } )
+    for (const practice of files) {
+      const fileName = path + practice
+      const content = fs.readFileSync(fileName, { encoding: 'utf8', flag: 'r' })
+      output.push(getEntitiesFromContent(content, patternsForEntities))
+    }
+  }
+  return output
+}
+
+for (const fileName in commonSearch) {
+  const content = fs.readFileSync(fileName, { encoding: 'utf8', flag: 'r' })
+  const object = {
+    objectID: fileName.replace('index.md', ''),
+    title: commonSearch[fileName].title,
+    keywords: commonSearch[fileName].summary,
+    tags: commonSearch[fileName].tags,
+    category: fileName.replace(/\/.+/g, ''),
+    practice: getPractice(fileName.replace('index.md', 'practice/'))
+  }
+  algoliaIndex.push(Object.assign(getEntitiesFromContent(content, patternsForEntities), object))
+}
+
+const client = algoliaClient(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY)
+const index = client.initIndex(process.env.ALGOLIA_APP_INDEX)
+
+index.saveObjects(algoliaIndex, { autoGenerateObjectIDIfNotExist: true })
