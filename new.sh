805db59555319c545597002ac4e22f5cc321ebda
Igor Korovchenko <igsekor@gmail.com>
2021-08-05 13:14:31 +0300

    Изменяет сообщение

--- new.sh
+++ new.sh
@@ -17,7 +17,7 @@ awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d 
 }
 LOGIN=$(echo $(curl -s https://api.github.com/search/users\?q\=$(echo $(git config --get user.email)) | jsonValue login))
 
-read -r -p "$(echo "Введите ник на GitHub (по умолчанию будет использован $LOGIN): ")" AUTHOR
+read -r -p "$(echo "Введите ник на GitHub (нажмите Enter, и будет использован $LOGIN): ")" AUTHOR
 
 if [[ $AUTHOR == "" ]]; then
   AUTHOR=$LOGIN
