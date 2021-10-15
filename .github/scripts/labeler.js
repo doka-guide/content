2c724f9a8043a141ae0c08a48ae6d7397467056f
Igor Korovchenko <igsekor@gmail.com>
2021-10-16 00:08:25 +0300

    Изменяет способ установки ярлыков

---
+++ .github/scripts/labeler.js
@@ -0,0 +1,49 @@
+const { Octokit } = require("@octokit/core")
+const fs = require('fs')
+
+const args = process.argv.slice(2)
+const ghKey = args.includes('--github-key') ? args[args.indexOf('--github-key') + 1] : false
+const pullNumber = args.includes('--pull-number') ? args[args.indexOf('--pull-number') + 1] : 0
+const owner = 'doka-guide'
+const repo = 'content'
+
+if (ghKey && pullNumber > 0) {
+  const rawLabelPatterns = fs.readFileSync('../../.labeler.json')
+  const labelPatterns = JSON.parse(rawLabelPatterns)
+
+  const octokit = new Octokit({ auth: ghKey })
+
+  const newLabels = ['эксперимент']
+
+  const pullObject = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+    owner,
+    repo,
+    pull_number: pullNumber
+  })
+
+  console.log(pullObject)
+
+  const fileObjects = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
+    owner,
+    repo,
+    pull_number: pullNumber
+  })
+
+  const files = {}
+
+  for (const index in fileObjects) {
+    if (Object.hasOwnProperty.call(fileObjects, index)) {
+      const file = fileObjects[index]
+      files[file.status].push[file.filename]
+    }
+  }
+
+  console.log(files)
+
+  await octokit.request('PATCH /repos/{owner}/{repo}/issues/{issue_number}', {
+    owner,
+    repo,
+    issue_number: pullNumber,
+    labels: newLabels
+  })
+}
