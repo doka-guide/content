746d5b87e369bdaed091cea0570b30e7b5af787b
Igor Korovchenko <igsekor@gmail.com>
2021-10-17 17:12:39 +0300

    Изменяет способ установки ярлыков (#1955)
    * Изменяет способ установки ярлыков
    
    * Добавляет загрузку файлов репозитория
    
    * Управляет асинхронностью
    
    * Добавляет установку модулей
    
    * Добавляет аргументы
    
    * Меняет путь
    
    * Исправляет работу с массивами
    
    * Добавляет дебаг
    
    * Убирает лишнюю проверку
    
    * Ещё дебаг
    
    * Добавляет структуру в когфигурацию
    
    * Задаёт список ярлыков
    
    * Немного дебажит
    
    * Пробует другой подход
    
    * Проверяет список файлов
    
    * Нащупывает хорошую структуру
    
    * Меняет структуру
    
    * Добавляет работу с лейблами для файлов
    
    * Дебаг
    
    * Дебаг
    
    * Дебаг
    
    * Дебаг
    
    * Пробует вариант
    
    * Добавляет вывод в консоль
    
    * Дебаг
    
    * Дебаг
    
    * Меняет паттерны
    
    * Оптимизирует скрипт
    
    * Исправляет ошибку
    
    * Исправляет ошибку
    
    * Убирает логирование
    
    * Добавляет правила для меты
    
    * Исправляет условие
    
    * Исправляет имя переменной
    
    * Удаляет артефакт из хелпа

---
+++ .github/scripts/labeler.js
@@ -0,0 +1,114 @@
+const { Octokit } = require("@octokit/core")
+const fm = require('front-matter')
+const fs = require('fs')
+
+const args = process.argv.slice(2)
+const ghKey = args.includes('--github-key') ? args[args.indexOf('--github-key') + 1] : false
+const pullNumber = args.includes('--pull-number') ? args[args.indexOf('--pull-number') + 1] : 0
+const owner = 'doka-guide'
+const repo = 'content'
+
+const selectLabels = (selectedFiles, selectedRules) => {
+  const output = new Set([])
+  for (const label in selectedRules) {
+    if (Object.hasOwnProperty.call(selectedRules, label)) {
+      const labelRules = selectedRules[label]
+      for (const status in labelRules) {
+        if (Object.hasOwnProperty.call(labelRules, status)) {
+          const statusRules = labelRules[status]
+          statusRules.forEach(pattern => {
+            if (Object.keys(selectedFiles).includes(status)) {
+              const regExp = new RegExp(pattern, 'i')
+              selectedFiles[status].forEach(file => {
+                const isValid = regExp.test(file)
+                const isNotInList = output.has(label)
+                if (isValid && isNotInList) {
+                  output.add(label)
+                }
+              })
+            }
+          })
+        }
+      }
+    }
+  }
+  return output
+}
+
+const setupLabels = async (ghKey, pullNumber) => {
+  if (ghKey && pullNumber > 0) {
+    const rawLabelRules = fs.readFileSync('.labeler.json')
+    const labelRules = JSON.parse(rawLabelRules)
+
+    const octokit = new Octokit({ auth: ghKey })
+
+    const pullObject = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+      owner,
+      repo,
+      pull_number: pullNumber
+    })
+
+    const labels = new Set([])
+    for (const index in pullObject.data.labels) {
+      const labelObject = pullObject.data.labels[index]
+      labels.add(labelObject.name)
+    }
+
+    const fileObjects = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
+      owner,
+      repo,
+      pull_number: pullNumber
+    })
+
+    const files = {
+      added: [],
+      modified: [],
+      removed: []
+    }
+
+    for (const index in fileObjects.data) {
+      const file = fileObjects.data[index]
+      files[file.status].push(file.filename)
+      if (Object.keys(labelRules).includes('meta') && (new RegExp('.+.md', 'i')).test(file.filename)) {
+        const content = fs.readFileSync(file.filename, { encoding: 'utf8', flag: 'r' })
+        const contentMeta = fm(content)
+        for (const field in labelRules.meta) {
+          if (Object.hasOwnProperty.call(labelRules.meta, field)) {
+            const fieldRules = labelRules.meta[field]
+            if (Object.keys(contentMeta).includes(field)) {
+              const metaSelectedLabels = selectLabels([file.filename], fieldRules.files)
+              metaSelectedLabels.forEach(element => {
+                labels.add(element)
+              })
+            }
+          }
+        }
+      }
+    }
+
+    const fileSelectedLabels = selectLabels(files, labelRules.files)
+    fileSelectedLabels.forEach(element => {
+      labels.add(element)
+    })
+
+    if (Object.keys(pullObject).includes('assignee')) {
+      pullObject.assignee.forEach(person => {
+        if (Object.keys(labelRules.assignee).includes(person)) {
+          const assigneeSelectedLabel = selectLabels(files, labelRules.assignee[person])
+          assigneeSelectedLabel.forEach(element => {
+            labels.add(element)
+          })
+        }
+      })
+    }
+
+    await octokit.request('PATCH /repos/{owner}/{repo}/issues/{issue_number}', {
+      owner,
+      repo,
+      issue_number: pullNumber,
+      labels: [...labels]
+    })
+  }
+}
+
+setupLabels(ghKey, pullNumber)
