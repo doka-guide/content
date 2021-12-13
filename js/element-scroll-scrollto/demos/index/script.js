0feaeb841444e7cad07b0f0d1604c8ce83dfcfb7
Nikolai Lopin <code@lopin.me>
2021-12-13 19:19:27 +0100

    let → const

--- js/element-scroll-scrollto/demos/index/script.js
+++ js/element-scroll-scrollto/demos/index/script.js
@@ -1,10 +1,10 @@
+const t = document.querySelector('.scrollable')
+
 function scrollPx(){
-  let t = document.querySelector('.scrollable')
   t.scrollTo(0, 200)
 }
 
 function scrollPercent(){
-  let t = document.querySelector('.scrollable')
-  let half = t.scrollHeight / 2
+  const half = t.scrollHeight / 2
   t.scrollTo(0, Math.ceil(half))
 }
