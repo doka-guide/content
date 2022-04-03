0474c4596c68b383e2529343b96653914b606bec
Igor Korovchenko <igsekor@gmail.com>
2022-04-03 17:57:15 +0300

    Добавляет сборку и публикацию поискового индекса

--- .github/scripts/search-index.js
+++ .github/scripts/search-index.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
-const algoliaClient = require('algoliasearch')
 const markdownIt = require('markdown-it')
 const { parseHTML } = require('linkedom')
 const { escape } = require('html-escaper')
+const natural = require('natural')
 
 const md = markdownIt({
   html: true,
@@ -10,14 +10,18 @@ const md = markdownIt({
   linkify: true,
 })
 
-const rawSearch = fs.readFileSync('search.json')
+const rawSearch = fs.readFileSync('search-meta.json')
 const commonSearch = JSON.parse(rawSearch)
-const algoliaIndex = []
 
+const TERM_FREQ_PREFIX_LIMIT = 10
 const stopCategories = [
-  'people',
-  'pages'
+  '^people',
+  '^pages'
 ]
+
+const exportObject = (json, fileName) => {
+  fs.writeFileSync(`${fileName}`, JSON.stringify(json), 'utf8')
+}
 
 const getEntitiesFromContent = (text) => {
   text = text.replace(/---(.|\n)*?---\n/g, '') // Вырезаем мету
@@ -47,43 +51,118 @@ const getPractice = (path) => {
   return []
 }
 
-for (const fileName in commonSearch) {
-  const content = fs.readFileSync(fileName, {
-    encoding: 'utf8',
-    flag: 'r'
-  })
+const buildContentObject = (commonSearch) => {
+  const contentObject = []
+  for (const fileName in commonSearch) {
+    const content = fs.readFileSync(fileName, {
+      encoding: 'utf8',
+      flag: 'r'
+    })
 
-  if (new RegExp(stopCategories.join('|')).test(fileName)) {
-    console.log(`Файл ${fileName} не будет добавлен в индекс.`)
-  } else {
-    const contentEntities = getEntitiesFromContent(content)
-    const practicesEntities = getPractice(fileName.replace('index.md', 'practice/'))
+    if (new RegExp(stopCategories.join('|')).test(fileName)) {
+      console.log(`Файл ${fileName} не будет добавлен в индекс.`)
+    } else {
+      const contentEntities = getEntitiesFromContent(content)
+      const practicesEntities = getPractice(fileName.replace('index.md', 'practice/'))
 
-    contentEntities.push(...practicesEntities)
+      contentEntities.push(...practicesEntities)
 
-    const description = commonSearch[fileName].description
-    if (description) {
-      const descriptionHTML = md.render(description)
-      const { document } = parseHTML(descriptionHTML)
-      const descriptionText = Array.from(document.children)
-        .map(element => element.textContent)
-        .join('')
-      contentEntities.unshift(escape(descriptionText))
+      const description = commonSearch[fileName].description
+      if (description) {
+        const descriptionHTML = md.render(description)
+        const { document } = parseHTML(descriptionHTML)
+        const descriptionText = Array.from(document.children)
+          .map(element => element.textContent)
+          .join('')
+        contentEntities.unshift(escape(descriptionText))
+      }
+
+      const object = {
+        objectID: fileName.replace('index.md', ''),
+        title: escape(commonSearch[fileName].title || ''),
+        keywords: commonSearch[fileName].keywords,
+        tags: commonSearch[fileName].tags,
+        category: fileName.replace(/\/.+/g, ''),
+        content: contentEntities,
+      }
+      contentObject.push(object)
     }
+  }
+  return contentObject
+}
 
-    const object = {
-      objectID: fileName.replace('index.md', ''),
-      title: escape(commonSearch[fileName].title || ''),
-      keywords: commonSearch[fileName].keywords,
-      tags: commonSearch[fileName].tags,
-      category: fileName.replace(/\/.+/g, ''),
-      content: contentEntities,
+const buildSearchOject = (contentObject) => {
+  const tokenizer = new natural.WordTokenizer()
+  const TfIdf = natural.TfIdf
+  const tfidf = new TfIdf()
+  const accumulator = []
+  const stems = []
+  const titles = []
+  for (const page of contentObject) {
+    const tokens = []
+    if (page.title) {
+      titles.push(page.title)
+      tokens.push(...tokenizer.tokenize(page.title))
     }
-    algoliaIndex.push(object)
+    if (page.keywords) {
+      tokens.push(...tokenizer.tokenize(page.keywords.join(' ')))
+    }
+    if (page.content) {
+      const joinedContent = page.content.join('\n')
+      tokens.push(...tokenizer.tokenize(joinedContent))
+      tfidf.addDocument(joinedContent)
+    }
+    accumulator.push(...new Set(tokens))
+    for (const t of tokens) {
+      stems.push(natural.PorterStemmerRu.stem(t))
+    }
+  }
+
+  return {
+    length: contentObject.length,
+    searchSet: [...new Set(accumulator)],
+    stems: [...new Set(stems)],
+    titles: titles,
+    tfidf: tfidf
   }
 }
 
-const client = algoliaClient(process.env.ALGOLIA_APP_ID, process.env.ALGOLIA_API_KEY)
-const index = client.initIndex(process.env.ALGOLIA_APP_INDEX)
+const buildSearchIndex = (searchObject) => {
+  const searchIndex = {}
+  for (const term of searchObject.searchSet) {
+    const raw = []
+    for (let i = 0; i < searchObject.length; i++) {
+      const freq = searchObject.tfidf.tfidf(term, i)
+      if (freq > 0) {
+        raw.push({ d: i, f: freq })
+      }
+    }
+    searchIndex[term.toLowerCase()] = raw.sort((a, b) => b.f - a.f).slice(0, TERM_FREQ_PREFIX_LIMIT)
+  }
+  return searchIndex
+}
 
-index.replaceAllObjects(algoliaIndex)
+const buildSearchTree = (searchObject, searchIndex) => {
+  const searchTree = {}
+  for (let stem of searchObject.stems) {
+    const processedTree = {}
+    for (const term of searchObject.searchSet) {
+      if (typeof term === 'string') {
+        const processedTerm = term.toLowerCase()
+        if (processedTerm.includes(stem)) {
+          processedTree[processedTerm] = searchIndex[processedTerm]
+        }
+      }
+    }
+    searchTree[stem] = processedTree
+  }
+  return searchTree
+}
+
+const contentObject = buildContentObject(commonSearch)
+const searchObject = buildSearchOject(contentObject)
+const searchIndex = buildSearchIndex(searchObject)
+const searchTree = buildSearchTree(searchObject, searchIndex)
+
+exportObject(contentObject, 'search-content.json')
+exportObject(searchTree, 'search-tree.json')
