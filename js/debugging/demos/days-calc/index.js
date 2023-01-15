7bda06702573d2a7d65397a75ee47c7372b3397c
alexbaumgertner <alex.baumgertner@gmail.com>
2023-01-15 22:55:22 +0300

    Добавляет пример кода для отладки

--- js/debugging/demos/days-calc/index.js
+++ js/debugging/demos/days-calc/index.js
@@ -7,19 +7,53 @@ let name = 'John'
 
 console.log(name)
 
-
-/// Пример 1
-/*
+// Пример 1
+/**
 * Синхронный код
 * Несколько функций, одна передается в другую в качестве коллбека
 * складывать числа, ошибка в приведении типов
 *
+* @param taxInPercent
+* @returns {function(*): *}
 * */
+function configureNetSalaryCalculator (taxInPercent) {
+  return function (salary) {
+    return salary - (salary * (taxInPercent / 100))
+  }
+}
 
-/// Пример 2
+const netSalaryCalculator = configureNetSalaryCalculator(35)
+const netSalary = netSalaryCalculator('4545,80')
 
-/*
-*  Асинхронный код (Promise)
-*
-* Promise + timeout
+console.log('netSalary: ', netSalary) // NaN
+
+// Пример 2
+
+/**
+ * Асинхронный код
+ * @param countryCode
+ * @returns {Promise<unknown>}
  */
+const requestTaxesByCountyMock = (countryCode) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      switch (countryCode) {
+        case 'AB':
+          resolve(5)
+          break
+        case 'CD':
+          resolve(15)
+          break
+        case 'REJECT':
+          reject('Country Code error')
+          break
+        default:
+          resolve(35)
+      }
+    }, 3000)
+  })
+}
+
+const taxValue = requestTaxesByCountyMock('AB')
+  .then((res) => {})
+  .catch(err => {})
