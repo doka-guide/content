e0371c2d5ad729a82d8842ba5ec5570f89c86a93
Igor Korovchenko <igsekor@gmail.com>
2024-07-14 18:16:17 +0300

    Исправляет работу экшена для добавления в ченджлог

--- .github/scripts/update-changelog.sh
+++ .github/scripts/update-changelog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git fetch origin main
-git diff --name-status origin/main HEAD | grep '^A' | grep -E '(html|css|js|tools|a11y|recipies)\/.*\/index\.md' | sed 's/^A\t//g' > added_dirs.txt
+git diff --name-status origin/main HEAD | grep '^A' | grep -E '(html|css|js|tools|a11y|recipes)\/.*\/index\.md' | sed 's/^A\t//g' > added-content.txt
 
-month_to_rus() {
+MONTH_TO_RUS() {
   case "$1" in
     01) echo "января" ;;
     02) echo "февраля" ;;
@@ -19,37 +19,16 @@ month_to_rus() {
   esac
 }
 
-merge_date=$(date -u +"%d %m")
-day=$(echo $merge_date | cut -d ' ' -f 1)
-month=$(echo $merge_date | cut -d ' ' -f 2)
-rus_month=$(month_to_rus $month)
-formatted_date="${day} ${rus_month}"
+MERGE_DATE=$(date -u +"%d %m")
+DAY=$(echo $MERGE_DATE | cut -d ' ' -f 1)
+MONTH=$(echo $MERGE_DATE | cut -d ' ' -f 2)
+RUS_MONTH=$(MONTH_TO_RUS $MONTH)
+FORMATTED_DATE="$DAY $RUS_MONTH"
 
-if [ -s added_dirs.txt ]; then
-  while IFS= read -r dir; do
-    path=${dir%/*}
-    new_folder="${dir##*/}"
-    if [ -f "${path}/${new_folder}/index.md" ]; then
-      title=$(sed -n 's/^title: \(.*\)/\1/p' "${path}/${new_folder}/index.md")
-      authors=$(sed -n 's/^authors: \[?\(.*\)\]?/\\1/p' "${path}/${new_folder}/index.md")
-      author_links=()
-      IFS=',' read -ra ADDR <<< "$authors"
-      for author in "${ADDR[@]}"; do
-        trimmed_author=$(echo $author | xargs)
-        if [ -d "people/$trimmed_author" ] && [ -f "people/$trimmed_author/index.md" ]; then
-          author_name=$(sed -n 's/^name: \(.*\)/\1/p' "people/$trimmed_author/index.md")
-          author_links+=("[${author_name}](https://doka.guide/people/${trimmed_author}/)")
-        else
-          author_links+=("[${trimmed_author}](https://doka.guide/people/${trimmed_author}/)")
-        fi
-      done
-      authors_string=$(IFS=, ; echo "${author_links[*]}")
-      echo "- ${formatted_date}, [${title}](https://doka.guide/${path#*/}/${new_folder}/), ${authors_string}" >> CHANGELOG.new.md
-    fi
-  done < added_dirs.txt
-  # git config --local user.email "<hi@doka.guide>"
-  # git config --local user.name "Doka Dog"
-  # git add CHANGELOG.md
-  # git commit -m "Обновляет CHANGELOG"
-  # git push
-fi
+node .github/scripts/update-changelog.js "$FORMATTED_DATE"
+
+git config --local user.email "<hi@doka.guide>"
+git config --local user.name "Doka Dog"
+git add CHANGELOG.md
+git commit -m "Обновляет CHANGELOG"
+git push
