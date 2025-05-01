350ab31823b15d41a97493cf6cee48397f698576
Doka Dog <hi@doka.guide>
2025-05-01 16:58:32 +0300

    Правит скрипты для добавления материалов в CHANGELOG

--- .github/scripts/update-changelog.sh
+++ .github/scripts/update-changelog.sh
@@ -1,32 +1,9 @@
 #!/bin/bash
-git diff --name-status HEAD^ HEAD | grep '^A' | grep -E '(html|css|js|tools|a11y|recipes)\/.*\/index\.md' | sed 's/^A\t//g' > added-content.txt
-git diff -S"placeholder" --name-status HEAD^ HEAD | grep '^M' | grep -E '(html|css|js|tools|a11y|recipes)\/.*\/index\.md' | sed 's/^M\t//g' > updated-content.txt
+git diff --name-status HEAD^...HEAD | grep '^A' | grep -E '(html|css|js|tools|a11y|recipes)\/.*\/index\.md' | sed 's/^A\t//g' > added-content.txt
+git diff -S"placeholder" --name-status HEAD^...HEAD | grep '^M' | grep -E '(html|css|js|tools|a11y|recipes)\/.*\/index\.md' | sed 's/^M\t//g' > updated-content.txt
 
 if [ -s added-content.txt ]; then
-    MONTH_TO_RUS() {
-    case "$1" in
-        01) echo "января" ;;
-        02) echo "февраля" ;;
-        03) echo "марта" ;;
-        04) echo "апреля" ;;
-        05) echo "мая" ;;
-        06) echo "июня" ;;
-        07) echo "июля" ;;
-        08) echo "августа" ;;
-        09) echo "сентября" ;;
-        10) echo "октября" ;;
-        11) echo "ноября" ;;
-        12) echo "декабря" ;;
-    esac
-    }
-
-    MERGE_DATE=$(date -u +"%d %m")
-    DAY=$(echo $MERGE_DATE | cut -d ' ' -f 1)
-    MONTH=$(echo $MERGE_DATE | cut -d ' ' -f 2)
-    RUS_MONTH=$(MONTH_TO_RUS $MONTH)
-    FORMATTED_DATE="$DAY $RUS_MONTH"
-
-    node .github/scripts/update-changelog.js "$FORMATTED_DATE"
+    node .github/scripts/update-changelog.js
 
     rm -f added-content.txt
     rm -f updated-content.txt
